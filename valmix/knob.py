a2a2e2013f117e0720e570342a3710a40e0e85c2
Stéphane Caron <stephane.caron@inria.fr>
2024-04-30 06:18:36 +0200

    Make knobs stateful, managing their current value

--- valmix/knob.py
+++ valmix/knob.py
@@ -14,14 +14,42 @@ class Knob:
     Attributes:
         name: Display name of the value.
         value: Actual multiprocessing synchronized shared object.
-        choices: Values the internal value may take.
+        values: Values the internal value may take.
     """
 
     name: str
     value: mp.Value
-    choices: Sequence
+    values: Sequence
 
-    def __init__(self, name: str, value: mp.Value, choices: Sequence):
+    def __init__(self, name: str, value: mp.Value, values: Sequence):
+        nb_values = len(values)
         self.name = name
         self.value = value
-        self.choices = sorted(choices)
+        self.values = sorted(values)
+        self.__index = nb_values // 2
+        self.__nb_values = nb_values
+
+    def advance(self, step: int):
+        self.__index += step
+        if self.__index >= self.__nb_values:
+            self.__index = self.__nb_values - 1
+        elif self.__index < 0:
+            self.__index = 0
+
+    @property
+    def current_index(self):
+        return self.__index
+
+    @property
+    def current_value(self):
+        return self.values[self.__index]
+
+    @property
+    def nb_values(self) -> int:
+        return self.__nb_values
+
+    def __repr__(self) -> str:
+        v = self.current_value
+        if isinstance(v, float):
+            return f"{v:.2}" if 1e-2 <= v < 100.0 else f"{v:.1e}"
+        return str(self.current_value)
