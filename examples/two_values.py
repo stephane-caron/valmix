513b20381ae03dd5a4272092d74f3e04e008521b
Stéphane Caron <stephane.caron@inria.fr>
2024-05-15 21:21:07 +0200

    Update example to the current API

--- examples/two_values.py
+++ examples/two_values.py
@@ -17,6 +17,7 @@ import numpy as np
 import valmix
 
 if __name__ == "__main__":
-    foo = valmix.Knob("foo", mp.Value("i"), range(-10, 10, 3))
-    bar = valmix.Knob("bar", mp.Value("f"), np.arange(-1.0, 3.0, 0.1))
-    valmix.run([foo, bar])
+    valmix.run({
+        "foo": (mp.Value("i", 0), range(-10, 10, 3)),
+        "bar": (mp.Value("f", 1.0), np.arange(-1.0, 3.0, 0.1)),
+    })
