cceb8ff6120ed7700eecfa2db8b1e838efa48d66
Stéphane Caron <stephane.caron@inria.fr>
2025-02-20 15:12:53 +0100

    docs: Update syntax highlighting configuration

--- docs/conf.py
+++ docs/conf.py
@@ -9,11 +9,8 @@ from os.path import abspath, dirname, join
 
 sys.path.insert(0, abspath(".."))
 
-# -- General configuration ------------------------------------------------
-
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
@@ -45,7 +42,7 @@ author = "Stéphane Caron"
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
-#
+
 # The short X.Y version.
 version = None
 
@@ -71,13 +68,8 @@ language = "en"
 # This patterns also effect to html_static_path and html_extra_path
 exclude_patterns = ["build", "Thumbs.db", ".DS_Store"]
 
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
-
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
-
-# -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
